a449fd452fd7ea513c8aecddaae4d9078e4aaa1d
Ariell C Onay <ariell.onay.931@gmail.com>
2026-04-07 22:10:20 +0200

    Update webcontainer.config.js

--- webcontainer.config.js
+++ webcontainer.config.js
@@ -1,7 +1,7 @@
 // WebContainer API Configuration
 
 const config = {
-    clientId: 'YOUR_CLIENT_ID_HERE', // Initialize with your client ID
+    clientId: 'wc_api_onay931_e91cf29d08cf42094b88bd4c4eb3ecd3', // Initialize with your client ID
     // Add other WebContainer configurations here
 };
 
